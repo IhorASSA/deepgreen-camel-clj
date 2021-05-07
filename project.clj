98e9e42a81029f7b00b1b36a67a910a2ebbc269c
Arsen A. Gutsal <gutsal.arsen@gmail.com>
2021-05-07 08:59:17 +0300

    `camel-exec` added. code improved

--- project.clj
+++ project.clj
@@ -3,11 +3,13 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
                  [takeoff/clj-camel "1.6.4"]
                  [org.apache.camel/camel-ftp "3.8.0"]
                  [org.apache.camel/camel-log "3.8.0"]
-                 [log4j "1.2.14"]
+                 ;; https://mvnrepository.com/artifact/org.apache.camel/camel-exec
+                 [org.apache.camel/camel-exec "3.8.0"]
+                 [log4j "1.2.17"]
                  [commons-logging "1.2"]
                  [org.slf4j/slf4j-api "2.0.0-alpha1"]
                  [org.slf4j/slf4j-simple "2.0.0-alpha1"]]
--- src/deepgreen_camel_clj/core.clj
+++ src/deepgreen_camel_clj/core.clj
@@ -1,27 +1,48 @@
 (ns deepgreen-camel-clj.core
   (:gen-class)
-  (:require [clj-camel.core :refer :all]))
+  (:require [clj-camel.core :refer :all]
+            [clj-camel.util :as cu]
+            ;; [org.apache.camel.builder.endpoint.StaticEndpointBuilders :reference :all]
+            )
+  (:import (org.apache.camel.component.exec ExecBinding)
+           (org.apache.commons.net.ftp FTPClientConfig)))
+
+;; (def ftp-client
+;;   (bean))
 
 (def route1 (route-builder
-             (from "ftp://ftp_exchange@194.44.29.184?password=rSac04jEr2ZuLXp4&passiveMode=true&move=.done")
+             (from "ftp://194.44.29.184?ftpClieasdfasdfntParameters=#ftp-client")
              (log "log ${in.header.CamelFileName}")
-             (to "file://target/")))
+             (to "file://target/")
+             (set-header ExecBinding/EXEC_COMMAND_ARGS (simple "${in.header.simple.CamelFileNameProduced}"))
+
+             (to "exec://file-importer.py")))
 
 (def route2 (route-builder
-             (from "timer://foo?fixedRate=true&period=60")
-             (set-body (constant "test"))
-             (throttle 2 {:async-delayed      false
-                          :reject-execution   false
-                          :time-period-millis 5000})
-             (log "${body} after throttling")
-             (to "seda:result")))
+             (from "timer://foo?fixedRate=true&period=1000")
+             (set-header ExecBinding/EXEC_COMMAND_ARGS (constant 'echo.py'))
+             (to "exec://echo.py")
+             (log "executed: ${body}")))
+             ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+             ;; (from "timer://heartbeat?period=60")          ;;
+             ;; (set-body (constant "test"))                  ;;
+             ;; (throttle 2 {:async-delayed      false        ;;
+             ;;              :reject-execution   false        ;;
+             ;;              :time-period-millis 5000})       ;;
+             ;; (log "${body} after throttling")              ;;
+             ;; (to "seda:result")))                          ;;
+             ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(def ^:dynamic ctx)
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (let [ctx (camel-context)]
+  (
+    (binding [ctx (camel-context )]
     (println "I don't do a whole lot ... yet.")
     (add-routes ctx route1)
     (.start ctx)
-    (Thread/sleep 10000)
-    (.shutdown ctx)))
+
+    ;; (Thread/sleep 5000)
+    ;; (.shutdown ctx)
+    )))
