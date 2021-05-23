4d1bbef4dfaf9d2520d358da67db21d5f5e46f44
Arsen A. Gutsal <gutsal.arsen@gmail.com>
2021-05-23 09:22:29 +0300

    FTP_*  replaced with INPUT_SOURCE string in .env

--- src/deepgreen_camel_clj/core.clj
+++ src/deepgreen_camel_clj/core.clj
@@ -8,13 +8,8 @@
   (:import (org.apache.camel.component.exec ExecBinding)
            (org.apache.commons.net.ftp FTPClientConfig)))
 
-(defn ftp
-  ""
-  [^String host ^String username ^String password]
-  (str "ftp://" host "?passiveMode=true&move=.done&username=" username "&password=" password))
-
 (def route1 (route-builder
-             (from (ftp (env 'FTP_HOST) (env 'FTP_USER) (env 'FTP_PASS)))
+             (from (env 'INPUT_SOURCE))
              (to "file://target/")
              (set-header ExecBinding/EXEC_COMMAND_ARGS (simple "${in.header.CamelFileNameProduced}"))
              (log "Stored locally: ${in.header.CamelFileNameProduced}")
@@ -25,8 +20,8 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Starting main routes")
-   (add-routes ctx route1)
-   (.start ctx)
+  (add-routes ctx route1)
+  (.start ctx)
 
     ;; (Thread/sleep 5000)
     ;; (.shutdown ctx)
